cce380e8824f227f3dd01c38a40dae81246da429
Lukasz Foj <lfoj@sigma.ug.edu.pl>
2011-11-06 12:32:38 +0100

    el 1 zad 3

---
+++ lukas.c
@@ -0,0 +1,13 @@
+/* Program ten wypisuje liczby z tablicy tabela[]
+   w odwrotnej kolejności: 12, 6, 4, 2, 1 */
+#include <stdio.h>
+int main() {
+  int i, tabela[] = {1, 2, 4, 6, 12};
+  while (i<=0)
+    {
+      i=4;
+       printf("%d\n", tabela[i]);
+       i--;  
+}
+   return 0;
+}
