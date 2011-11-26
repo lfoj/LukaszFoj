bfbac38da9d1591da7120bec8df5b6a8ec3dea95
Lukasz Foj <lfoj@sigma.ug.edu.pl>
2011-11-26 11:16:49 +0100

    Zadanie 4 z pętli

---
+++ pętle_zad4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, suma = 0, a;
+
+  printf("Podaj liczbę do której będziemy sumować: ");
+  scanf("%d", &a);
+
+  for (i = 1; i<=a; ++i)
+    {
+      if (a<1) {
+	printf("Ta liczba jest nieprawidłowa.\n");
+           
+ }
+ 
+      
+      suma += i;
+    }
+  printf("%d\n", suma);
+  return 0;
+}
