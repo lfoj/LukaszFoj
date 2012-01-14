7d2de880812771fb57ec533e5fe7aec47faef660
Lukasz Foj <lfoj@sigma.ug.edu.pl>
2012-01-14 20:49:40 +0100

    Rekurencja

--- Rekurencja/rekur3.c
+++ Rekurencja/rekur3.c
@@ -1,21 +1,24 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-string funkcja(string in, char *napis);
+void funkcja(string in, char *napis);
 
 int main() {
   
 char *napis = "abcdef";
 
  printf("in = %s\n", napis);
+ 
  funkcja(napis);
+
  printf("\n");
   return 0;
 }
-void funkcja(string in) {
+void funkcja(string in, char *napis) {
+
   int i, len = StringLength(in);
   
-
   for (i = len - 1; i >= 0; i--)
     putchar(in[i]);
 
